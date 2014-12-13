c01a079789faf39f94cd8391baa0619ead315ba3
Lane <me@lanesawyer.com>
2014-12-13 14:40:41 -0600

    ranking system working

--- app.js
+++ app.js
@@ -13,31 +13,6 @@ var settings = {
 
 var ranking = new glicko2.Glicko2(settings);
 
-// Create players
-var Ryan = ranking.makePlayer();
-var Bob = ranking.makePlayer(1400, 30, 0.06);
-var John = ranking.makePlayer(1550, 100, 0.06);
-var Mary = ranking.makePlayer(1700, 300, 0.06);
-
-var matches = [];
-matches.push([Ryan, Bob, 1]); //Ryan won over Bob
-matches.push([Ryan, John, 0]); //Ryan lost against John
-matches.push([Ryan, Mary, 0.5]); //A draw between Ryan and Mary
-ranking.updateRatings(matches);
-
-console.log('Ryan new rating: ' + Ryan.getRating());
-console.log('Ryan new rating deviation: ' + Ryan.getRd());
-console.log('Ryan new volatility: ' + Ryan.getVol());
-
-
-function ExperiencePair(experience1, experience2) {
-  this.experience1 = experience1;
-  this.experience2 = experience2;
-  this.winner = 0.5;
-};
-
-
-
 var app = angular.module('gShe', ['ngRoute', 'firebase']);
 
 app.config(['$routeProvider', function($routeProvider){
@@ -49,47 +24,3 @@ app.config(['$routeProvider', function($routeProvider){
     controller: 'ExperienceController'
   });
 }]);
-
-app.controller('ExperienceController', function($scope, $firebase) {
-  var ref = new Firebase('https://g-she.firebaseio.com/experiences/');
-  var sync = $firebase(ref);
-
-  $scope.experiences = sync.$asArray();
-
-  $scope.batch = [];
-  $scope.eligibleExperiences = [];
-  $scope.experienceCounter = 0;
-
-  $scope.experiencePairs = [];
-  $scope.experiencePairCounter = 0;
-
-  $scope.hadExperience = function(experience) {
-    $scope.eligibleExperiences.push(experience);
-    $scope.nextExperience();
-  };
-
-  $scope.nextExperience = function() {
-    $scope.experienceCounter++;
-    if($scope.experienceCounter == $scope.experiences.length) {
-      $scope.allExperiencesAnswered = true;
-      $scope.experiencePairs = createExperiencePairs();
-    }
-  };
-
-  $scope.nextExperiencePair = function(experiencePair, winner) {
-    experiencePair.winner = winner;
-    $scope.experiencePairCounter++;
-  };
-
-  function createExperiencePairs() {
-    var experiencePairs = [];
-    
-    for(var i = 0; i < $scope.eligibleExperiences.length; i++) {
-      for(var j = i + 1; j < $scope.eligibleExperiences.length; j++) {
-        experiencePairs.push(new ExperiencePair($scope.eligibleExperiences[i], $scope.eligibleExperiences[j]));
-      }
-    }
-
-    return experiencePairs;
-  };
-});
---
+++ experienceController.js
@@ -0,0 +1,80 @@
+function ExperiencePair(experience1, experience2) {
+  this.experience1 = experience1;
+  this.experience2 = experience2;
+  this.winner = 0.5;
+};
+
+app.controller('ExperienceController', function($scope, $firebase) {
+  var ref = new Firebase('https://g-she.firebaseio.com/experiences/');
+  var sync = $firebase(ref);
+
+  $scope.experiences = sync.$asArray();
+
+  $scope.batch = [];
+  $scope.eligibleExperiences = [];
+  $scope.experienceCounter = 0;
+
+  $scope.experiencePairs = [];
+  $scope.experiencePairCounter = 0;
+
+  $scope.experiencePlayerSet = {};
+
+  $scope.hadExperience = function(experience) {
+    $scope.eligibleExperiences.push(experience);
+    $scope.nextExperience();
+  };
+
+  $scope.nextExperience = function() {
+    $scope.experienceCounter++;
+    if($scope.experienceCounter == $scope.experiences.length) {
+      $scope.allExperiencesAnswered = true;
+      $scope.experiencePairs = createExperiencePairs();
+    }
+  };
+
+  $scope.nextExperiencePair = function(experiencePair, winner) {
+    experiencePair.winner = winner;
+
+    var experience1 = createPlayerFromExperience(experiencePair.experience1);
+    var experience2 = createPlayerFromExperience(experiencePair.experience2);
+
+    $scope.batch.push([experience1, experience2, winner]);
+
+    $scope.experiencePairCounter++;
+    if($scope.experiencePairCounter == $scope.experiencePairs.length) {
+      ranking.updateRatings($scope.batch);
+
+      updateExperiences();
+    }
+  };
+
+  function createExperiencePairs() {
+    var experiencePairs = [];
+    
+    for(var i = 0; i < $scope.eligibleExperiences.length; i++) {
+      for(var j = i + 1; j < $scope.eligibleExperiences.length; j++) {
+        experiencePairs.push(new ExperiencePair($scope.eligibleExperiences[i], $scope.eligibleExperiences[j]));
+      }
+    }
+
+    return experiencePairs;
+  };
+
+  function createPlayerFromExperience(experience) {
+    if(!$scope.experiencePlayerSet[experience.$id]) {
+      $scope.experiencePlayerSet[experience.$id] = ranking.makePlayer(experience.rating, experience.rd, experience.vol);
+    }
+
+    return $scope.experiencePlayerSet[experience.$id];
+  };
+
+  function updateExperiences() {
+    angular.forEach($scope.experiencePlayerSet, function(value, key) {
+      var experience = $scope.experiences.$getRecord(key);
+      experience.rating = value.getRating();
+      experience.rd = value.getRd();
+      experience.vol = value.getVol();
+      $scope.experiences.$save(experience);
+    });
+  };
+});
