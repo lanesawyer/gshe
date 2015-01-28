1edb851f42395f9777abc435ae5776a82467e71d
Lane <me@lanesawyer.com>
2015-01-27 19:10:43 -0600

    Validation on create account page

--- authService.js
+++ authService.js
@@ -15,15 +15,7 @@ app.factory('authService', function($location, config) {
     if(error) {
       console.log('Login Failed!', error);
     } else {
-
-      if(newUser) {
-        var user = new User($scope.user);
-
-        ref.child('users').child(authData.uid).set(authData);
-      }
-
       $location.path('/userProfile');
-      console.log('Authenticated successfully with payload:', authData);
     }
   };
 
@@ -38,9 +30,11 @@ app.factory('authService', function($location, config) {
     if(error) {
       console.log('Account Creation Failed!', error);
     } else {
-      newUser = true;
-      authService.login();
-      console.log('Successfully created account.', authData);
+      var user = new User($scope.user);
+
+      ref.child('users').child(authData.uid).set(user);
+
+      $location.path('/login');
     }
   };
 
